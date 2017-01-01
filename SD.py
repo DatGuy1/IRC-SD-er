82e121371cba3d36ccaac00bf5963604ec8e4483
DatGuy1 <datguysteam@gmail.com>
2017-01-01 17:31:57 +0100

    Update SD.py

--- SD.py
+++ SD.py
@@ -1,5 +1,5 @@
 import irc
-import irc.bot
+#import irc.bot
 from irc.bot import SingleServerIRCBot
 from wikitools import *
 import thread
@@ -9,11 +9,16 @@ import time
 from datetime import datetime
 import os
 
+channel = "#wikipedia-en-csd"
+
 site = wiki.Wiki()
-site.login('DatBot','redacted')
-useAPI = True
+#site.login('DatBot','redacted')
+#useAPI = True
+useAPI = False
 ctitle = 'Category:Candidates for speedy deletion'
 connections = {}
+
+CSDcats = {"g1": "nonsense pages", "g10": "attack pages"}
 
 class timedTracker(dict):
     def __init__(self, args={}, expiry=300):
@@ -114,74 +119,86 @@ class CommandBot(SingleServerIRCBot):
         connections['command'] = c
         sendToChannel("Bot initialised")
         return
-	def on_pubmsg(self, c, e):
-		a = e.arguments[0]
-		stripped = e.arguments[0].split("!")
-		if len(a) > 1 and a[0] == "!":
-			self.do_command(e, stripped[1])
-		return
-	def do_command(self, e, cmd):
-		nick = e.source.nick
-		c = self.connection
-		if cmd == "quiet":
-			sendToChannel("/cs op #wikipedia-en-csd DatBotCSD")
-			time.sleep(0.25)
-			sendToChannel("/mode +q DatBotCSD!*@*")
-			time.sleep(0.25)
-			sendToChannel("/cs deop #wikipedia-en-csd DatBotCSD")
-			time.sleep(0.25)
-			sendToChannel("/cs devoice #wikipedia-en-csd DatBotCSD")
-		elif cmd == "unquiet":
-			sendToChannel("/cs op #wikipedia-en-csd DatBot")
-			time.sleep(0.25)
-			sendToChannel("/mode -q DatBotCSD!*@*")
-			time.sleep(0.25)
-			sendToChannel("/cs deop #wikipedia-en-csd DatBotCSD")
-			time.sleep(0.25)
-			sendToChannel("/cs voice #wikipedia-en-csd DatBotCSD")
-		#Use this format for other categories
-		#BEGIN:
-		elif cmd == "g1":
-			g1cat = category.Category(site, "Category:Candidates for speedy deletion as nonsense pages")
-			g1mem = g1cat.getAllMembets(titleonly = True, reload = True)
-			if g1mem == []:
-				sendToChannel("There are no candidates for speedy deletion as nonsense pages")
-			else:
-				for row in g1mem:
-					g1p = page.Page(site, row)
-					sendToChannel("%s - https://en.wikipedia.org/wiki/%s" %(g10p.title, g10p.urltitle))
-		#:END
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0]
+        stripped = e.arguments[0].split("!")
+        if len(a) > 1 and a[0] == "!":
+            self.do_command(e, stripped[1])
+        return
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+        if cmd == "quiet":
+            sendToChannel("/cs op #wikipedia-en-csd DatBotCSD")
+            time.sleep(0.25)
+            sendToChannel("/mode +q DatBotCSD!*@*")
+            time.sleep(0.25)
+            sendToChannel("/cs deop #wikipedia-en-csd DatBotCSD")
+            time.sleep(0.25)
+            sendToChannel("/cs devoice #wikipedia-en-csd DatBotCSD")
+        elif cmd == "unquiet":
+            sendToChannel("/cs op #wikipedia-en-csd DatBot")
+            time.sleep(0.25)
+            sendToChannel("/mode -q DatBotCSD!*@*")
+            time.sleep(0.25)
+            sendToChannel("/cs deop #wikipedia-en-csd DatBotCSD")
+            time.sleep(0.25)
+            sendToChannel("/cs voice #wikipedia-en-csd DatBotCSD")
+        #Use this format for other categories
+        #BEGIN:
+        # elif cmd == "g1":
+            # g1cat = category.Category(site, "Category:Candidates for speedy deletion as nonsense pages")
+            # g1mem = g1cat.getAllMembets(titleonly = True, reload = True)
+            # if g1mem == []:
+                # sendToChannel("There are no candidates for speedy deletion as nonsense pages")
+            # else:
+                # for row in g1mem:
+                    # g1p = page.Page(site, row)
+                    # sendToChannel("%s - https://en.wikipedia.org/wiki/%s" %(g10p.title, g10p.urltitle))
+        if cmd in CSDcats:
+            csdName = CSDcats[cmd]
+            csdCat = category.Category(site, "Category:Candidates for speedy deletion as %s" % csdName)
+            csdMem = csdCat.getAllMembers(titleonly = True, reload = True)
+            if not csdMem:
+                sendToChannel("There are no candidates for speedy deletion as %s." % csdName)
+            else:
+                for row in csdMem:
+                    csdPage = page.Page(site, row)
+                    sendToChannel("%s - https://en.wikipedia.org/wiki/%s" %(csdPage.title, csdPage.urltitle))
+        #:END
 def sendToChannel(msg):
-    connections['command'].privmsg("#wikipedia-en-csd", msg)
-	
+    connections['command'].privmsg(channel, msg)
+    
 class BotRunnerThread(threading.Thread):
     def __init__(self, bot):
         threading.Thread.__init__(self)
         self.bot = bot
     def run(self):
         self.bot.start()
-		
+        
 def getStart():
     if useAPI:
-	    params = {'action':'query',
-		    'list':'categorymembers',
+        params = {'action':'query',
+            'list':'categorymembers',
                     'utf8':'1',
                     'cmtitle':ctitle,
-		    'cmprop':'ids|timestamp',
-		    'cmlimit':'1',
-	    }
-	    req = api.APIRequest(site, params)
-	    res = req.query(False)
-	    row = res['query']['categorymembers'][0]
-	    lasttime = row['timestamp']
+            'cmprop':'ids|timestamp',
+            'cmlimit':'1',
+        }
+        req = api.APIRequest(site, params)
+        res = req.query(False)
+        row = res['query']['categorymembers'][0]
+        lasttime = row['timestamp']
+    else:
+        lasttime = 0
     return (lasttime)
 def main():
     global connections
-    Cchannel = "#wikipedia-en-csd"
-    channel = "#wikipedia-en-csd"
+    #channel = "#wikipedia-en-csd"
     Cserver = "irc.freenode.net"
     nickname = "DatBotCSD"
-    cbot = CommandBot(Cchannel, nickname, Cserver)
+    #cbot = CommandBot(Cchannel, nickname, Cserver)
+    cbot = CommandBot(channel, nickname, Cserver)
     cThread = BotRunnerThread(cbot)
     cThread.daemon = True
     cThread.start()
@@ -191,8 +208,9 @@ def main():
         time.sleep(2)
         print("In while")
     time.sleep(5)
+    rows = []
     while True:
-        (lasttime) = getStart()
+        lasttime = getStart()
         if useAPI:
             rows = logFromAPI(lasttime)
         for row in rows:
@@ -209,4 +227,4 @@ def main():
                 IRCreported[(ns,title)] = 1
                 
 if __name__ == "__main__":
-	main()
+    main()
